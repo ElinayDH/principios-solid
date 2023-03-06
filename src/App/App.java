b28c4535f591b020e466d06600ae16fd73051f5b
Elinay Diaz Home <diazhomeelinay@gmail.com>
2023-03-06 10:38:53 -0300

    Corrigindo o metodo Main

--- src/App/App.java
+++ src/App/App.java
@@ -1,10 +1,11 @@
-package srp;
+package App;
 
 public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		System.out.println("Executando código...!");
 		
 
  }
